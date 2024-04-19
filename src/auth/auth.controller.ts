93d0de4f5bdfbb57044884ae17e031e14ff7501d
Sebastian Rincon <serincont@gmail.com>
2024-04-19 00:11:10 -0500

    add guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/guards.guard';
 
 @Controller('auth')
+@UseGuards(AuthGuard)
 export class AuthController {
     constructor(
         private authService: AuthService
